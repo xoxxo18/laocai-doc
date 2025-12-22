11a443986868a331a5bddb0128c6500ed47df07c
Duncan Sin <2331630699@qq.com>
2025-12-22 22:08:16 +0800

    renamed:    app/[[...mdxPath]]/page.tsx -> app/[[...mdxPath]]/page.jsx

--- app/[[...mdxPath]]/page.jsx
+++ app/[[...mdxPath]]/page.jsx
@@ -3,7 +3,7 @@ import { useMDXComponents as getMDXComponents } from '../../mdx-components'
  
 export const generateStaticParams = generateStaticParamsFor('mdxPath')
  
-export async function generateMetadata(props: { params: Promise<{ mdxPath: string[] }> }) {
+export async function generateMetadata(props) {
   const params = await props.params
   const { metadata } = await importPage(params.mdxPath)
   return metadata
@@ -11,7 +11,7 @@ export async function generateMetadata(props: { params: Promise<{ mdxPath: strin
  
 const Wrapper = getMDXComponents().wrapper
  
-export default async function Page(props: { params: Promise<{ mdxPath: string[] }> }) {
+export default async function Page(props) {
   const params = await props.params
   const {
     default: MDXContent,
